3c893126505ae9cd23436b78ef229d33463f1986
SadiJr <sadi@gmail.com>
2024-10-19 16:15:10 -0300

    fix: fix again the cors

--- src/main/java/com/omegaeducacional/caixa/controller/UserController.java
+++ src/main/java/com/omegaeducacional/caixa/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
