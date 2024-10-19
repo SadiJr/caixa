86fad1e82b14a36eb73e97cbaef0e11b0edf0fd5
SadiJr <sadi@gmail.com>
2024-10-19 09:17:15 -0300

    feat: add initial project structure

---
+++ src/test/java/com/omegaeducacional/caixa/CaixaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.omegaeducacional.caixa;
+
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaApplicationTests {
+
+	@Ignore
+	@Test
+	void contextLoads() {
+	}
+
+}
